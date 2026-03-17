c21d20e9cb8f03f9c9a3ff31c96857f5b37778bd
Felipe Marzochi <fmarzochi@gmail.com>
2026-03-16 21:31:29 -0300

    Eu corrigi os caminhos de importação no Root Layout utilizando o alias absoluto do Next.js

--- frontend/web/app/layout.tsx
+++ frontend/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import "../styles/globals.css";
-import Navbar from "../components/navbar/Navbar";
-import Footer from "../components/footer/Footer";
+import "@/styles/globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 export const metadata: Metadata = {
   title: "Conecta 360º | Consultoria Integrada",
