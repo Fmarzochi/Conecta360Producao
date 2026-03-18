4ee9f7af5d443d74ae60010414af5cbf33022497
Matheus Fuentes <matheus.fu.p@hotmail.com>
2026-03-18 18:08:40 -0300

    feat: Implementado jest config no packages

---
+++ packages/database/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+  setupFiles: ["dotenv/config"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+};
