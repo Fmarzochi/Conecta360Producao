1367ed808000b9d2c77560e7fa41c60c844afca1
Matheus Fuentes <matheus.fu.p@hotmail.com>
2026-03-25 12:09:09 -0300

    feat: Incrementado auth guard ao AppModule

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { TenantInterceptor } from '@conecta360/database';
+import { AuthModule } from './modules/auth.module';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: TenantInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
     },
   ],
 })
