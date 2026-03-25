27f9e5ef1a6a385f947cd36831447b1f1a966e0b
Matheus Fuentes <matheus.fu.p@hotmail.com>
2026-03-25 12:02:51 -0300

    feat: Implementado validação DTOs global

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,13 +1,23 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { initializeApp } from '@conecta360/utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const expressApp = app.getHttpAdapter().getInstance();
   initializeApp(expressApp);
+
+  // Habilita validação global de DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
