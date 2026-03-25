8a1b6969de63207a7b1dc04eacd71eede9abcc27
Matheus Fuentes <matheus.fu.p@hotmail.com>
2026-03-25 14:57:06 -0300

    feat: Implementado pipeline + throttler + swagger para a aplicação e removido a validação do main

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { TenantInterceptor } from '@conecta360/database';
 import { AuthModule } from './modules/auth.module';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ValidationPipeOptions } from '@nestjs/common';
+
+export const validationPipeConfig: ValidationPipeOptions = {
+  whitelist: true,
+  forbidNonWhitelisted: true,
+  transform: true,
+};
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 15,
+    }]),
+  ],
   controllers: [],
   providers: [
     {
@@ -16,6 +30,10 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
     },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe(validationPipeConfig),
+    },
   ],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { initializeApp } from '@conecta360/utils';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,14 +11,15 @@ async function bootstrap() {
   const expressApp = app.getHttpAdapter().getInstance();
   initializeApp(expressApp);
 
-  // Habilita validação global de DTOs
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  const config = new DocumentBuilder()
+    .setTitle('Conecta360 - Auth Service')
+    .setDescription('API de Autenticação e Gestão de Tokens')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
