3f737d91afd6889172f7c40139d3c3faf7f3885b
kim-a9 <kimberlysantosalves@gmail.com>
2026-03-27 23:21:11 -0300

    fix: adicionei o lançamento de erro no bloco catch{} e logs

--- apps/user-service/src/services/CreateUserPJServices.ts
+++ apps/user-service/src/services/CreateUserPJServices.ts
@@ -5,32 +5,41 @@ import { UserModel } from '../entities/User';
 import { CompanyModel } from '../entities/Company';
 import { UserCompanyModel } from '../entities/UserCompany';
 import { TenantModel } from '../entities/Tenant';
+import { logger } from "../config/logger";
 
 
 export class CreateUserPJServices {
   async execute(data: any) {
     if (!cnpj.isValid(data.cnpj)) throw new Error("CNPJ Inválido");
     if (!cpf.isValid(data.user.cpf)) throw new Error("CPF do representante inválido");
+    logger.warn({ cpf: data.user.cpf }, 'Tentativa de cadastro com CPF inválido');
+    
 
     const session = await mongoose.startSession();
     session.startTransaction();
 
     try {
+      logger.info({ tradeName: data.tradeName, tenantId: data.tenantId }, 'Registro de novo usuário PJ');
+      
       const [newTenant] = await TenantModel.create([{ 
         name: data.tradeName,
         status: 'ACTIVE' 
       }], { session });
-
+      
       const tenantId = newTenant._id; 
+      logger.info({ tenantId: data.tenantId, tradeName: data.tradeName }, 'Tenant criado com sucesso');
 
       const passwordHash = await hash(data.user.password, 8);
-      const user = await UserModel.create([{
+      const [user] = await UserModel.create([{
         ...data.user,
         tenantId,
         passwordHash,
         role: 'ADMIN', 
         status: 'ACTIVE'
       }], { session });
+
+      logger.info({ tenantId: data.tenantId, role: data.user.role }, 'Usuário criado com sucesso');
+
 
       const [company] = await CompanyModel.create([{
         tenantId,
@@ -41,6 +50,8 @@ export class CreateUserPJServices {
         status: 'ACTIVE'
       }], { session });
 
+      logger.info({ legalName: data.legalName, corporateEmail: data.corporateEmail }, 'Company criado com sucesso');
+
       await UserCompanyModel.create([{
         tenantId,
         userId: data.user._id,
@@ -48,6 +59,7 @@ export class CreateUserPJServices {
         relationshipType: 'OWNER',
         status: 'ACTIVE'
       }], { session });
+      logger.info({ userId: data.user._id, companyId: company._id }, 'UserCompany criado com sucesso');
 
       await session.commitTransaction();
             
@@ -55,7 +67,8 @@ export class CreateUserPJServices {
 
     } catch (error: any) {
       await session.abortTransaction();
-      console.error("[ERRO] Cadastro cancelado. Motivo:", error.message);
+      logger.error({ error: error.message }, 'Falha ao registrar usuário PJ');
+      throw error;
     } finally {
       session.endSession();
     }
